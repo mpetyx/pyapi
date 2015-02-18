36ea9a9b09465ef06c1b8fc0b7e0e80298fdb39c
Michael Petychakis <mpetyx@gmail.com>
2015-02-18 16:53:28 +0200

    adding setup script for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+__author__ = 'mpetyx'
+
+from distutils.core import setup
+setup(name="pyapi",
+      description="pyapi Libray",
+      version="1.0",
+      author="Michael Petychakis",
+      author_email="mpetyx@epu.ntua.gr",
+	  maintainer="Michael Petychakis",
+	  maintainer_email="mpetyx@epu.ntua.gr",
+      packages=['pyapi'],
+	  requires = ['rdflib'])
