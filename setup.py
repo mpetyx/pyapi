5a4a5a5a98407672fdb5f62d73f2624947b5a4b0
Michael Petychakis <mpetyx@gmail.com>
2015-03-26 14:15:53 +0200

    changing installation version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = 'mpetyx'
 from distutils.core import setup
 setup(name="pyapi",
       description="pyapi Libray",
-      version="1.0",
+      version="1.1",
       author="Michael Petychakis",
       author_email="mpetyx@epu.ntua.gr",
 	  maintainer="Michael Petychakis",
